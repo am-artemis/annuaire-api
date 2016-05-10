7a7e839facf3537b583bffb846e90a3d7b59b63e
Corentin GITTON <corentin.gitton@gmail.com>
2016-05-10 22:32:21 +0200

    Modification gulpfile.js pour être compatible avec windows sans path + quelques améliorations

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,48 @@
-var elixir = require('laravel-elixir');
-
-
 // Static resources
-
-var elixir = require('laravel-elixir');
-
-elixir.config.sourcemaps = false;
-
-
-
-// Test launcher
-
 var gulp = require('gulp');
 var exec = require('child_process').exec;
 var notify = require("gulp-notify");
 var gutil = require('gulp-util');
-var phpunitBin = /^win/.test(process.platform) ? 'phpunit ' : './vendor/bin/phpunit ';
-var phpunitPath = '';
 
-gulp.task('phpunit', function() {
-    console.log('\n$ phpunit '+phpunitPath+'\n');
-    exec(phpunitBin+phpunitPath, function(error, stdout) {
-        console.log('\n'+stdout);
+// Elixir
+var elixir = require('laravel-elixir');
+elixir.config.sourcemaps = false;
+
+// Test function
+var phpunit_bin = /^win/.test(process.platform) ? 'vendor\\bin\\phpunit.bat' : './vendor/bin/phpunit';
+
+function phpunit(unit_test_path) {
+    unit_test_path = typeof unit_test_path !== 'undefined' ? unit_test_path : '';
+
+    console.log('\n$ phpunit ' + unit_test_path + '\n');
+
+    exec(phpunit_bin + ' ' + unit_test_path, function(error, stdout, stderr) {
+        console.log('\n' + stdout);
         if (! error) {
-            console.log(gutil.colors.bgGreen('                                                                                                            '));
+            console.log(gutil.colors.bgGreen( Array(108).join(' ')) );
         } else {
-            console.log(gutil.colors.bgRed('                                                                                                            '));
+            // console.log('\nERROR: ' + error);
+            console.log(gutil.colors.bgRed( Array(108).join(' ')) );
         }
-        console.log('\n============================================================================================================\n');
+        console.log(Array(108).join('=') + '\n');
     });
+}
+
+// Single test
+gulp.task('phpunit', function(a) {
+    console.log(a);
+    phpunit();
 });
 
+// Continuous testing
 gulp.task('tdd', function() {
-    var watcher = gulp.watch('tests/**/*.php', { debounceDelay: 1000 }, ['phpunit']);
-    watcher.on('change', function(event){
-        GLOBAL.phpunitPath = event.path.replace(/\\/g, '/');
+    var test_watcher = gulp.watch('tests/**/*.php', { debounceDelay: 1000 });
+    test_watcher.on('change', function(event){
+        phpunit( event.path.replace(/\\/g, '/') );
     });
 
-    var watcher = gulp.watch('app/**/*.php', { debounceDelay: 1000 }, ['phpunit']);
-    watcher.on('change', function(event){
-        GLOBAL.phpunitPath = event.path.replace(/\\/g, '/').replace('app', 'tests').replace('.php', 'Test.php');
+    var app_watcher = gulp.watch('app/**/*.php', { debounceDelay: 1000 });
+    app_watcher.on('change', function(event){
+        phpunit( event.path.replace(/\\/g, '/').replace('/app/', '/tests/').replace(/.php$/, 'Test.php') );
     });
 });
