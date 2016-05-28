d5519e7c25b9280608e807e38106b8a1f8273fc8
Mathieu TUDISCO <mathieu.tudisco@neoxia.com>
2016-05-28 11:11:22 +0200

    Authentification via google opérationnelle

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthStoreRequest;
 use App\Models\User;
+use Socialite;
+use Response;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\JWTAuth;
 
@@ -23,9 +25,28 @@ class AuthController extends Controller
         $this->middleware('jwt.refresh', ['only' => ['update']]);
     }
 
-    public function store(AuthStoreRequest $request)
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
     {
-        $email = $request->input('email');
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $providerUser = Socialite::driver('google')->stateless()->user();
+        return $this->store($providerUser->email);
+    }
+    public function store($email)
+    {
         try {
             // attempt to verify the credentials and create a token for the user
             $user = User::whereEmail($email)->first();
@@ -35,7 +56,7 @@ class AuthController extends Controller
             $token = $this->auth->fromUser($user);
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
-            $this->response->error('Could not create token', 500);
+            $this->response->error('Could not create token.', 500);
         }
 
         return response()->json(compact('user', 'token'));
